9586886ad176696fe3e5636123d55721dc871e57
szdp6dtp56-ux <szdp6dtp56@privaterelay.appleid.com>
2026-09-19 12:04:02 +0900

    Add offline support

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE = 'lift-note-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(saved => saved || fetch(event.request))));
+
